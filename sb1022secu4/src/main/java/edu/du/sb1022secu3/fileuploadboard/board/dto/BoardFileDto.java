2fc7c3dbe39c760396b039288ed652daf1bc1c19
unknown <19941128o@gmail.com>
2024-10-28 21:48:17 +0900

    commit

---
+++ sb1022secu4/src/main/java/edu/du/sb1022secu3/fileuploadboard/board/dto/BoardFileDto.java
@@ -0,0 +1,17 @@
+package edu.du.sb1022secu3.fileuploadboard.board.dto;
+
+import lombok.Data;
+
+@Data
+public class BoardFileDto {
+	
+	private int idx;
+	
+	private int boardIdx;
+	
+	private String originalFileName;
+	
+	private String storedFilePath;
+	
+	private long fileSize;
+}
---
+++ sb1022secu4/src/main/java/edu/du/sb1022secu3/fileuploadboard/board/service/BoardService.java
@@ -0,0 +1,23 @@
+package edu.du.sb1022secu3.fileuploadboard.board.service;
+
+
+import edu.du.sb1022secu3.fileuploadboard.board.dto.BoardDto;
+import edu.du.sb1022secu3.fileuploadboard.board.dto.BoardFileDto;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.util.List;
+
+public interface BoardService {
+	
+	List<BoardDto> selectBoardList() throws Exception;
+	
+	void insertBoard(BoardDto board, MultipartHttpServletRequest multipartHttpServletRequest) throws Exception;
+
+	BoardDto selectBoardDetail(int boardIdx) throws Exception;
+
+	void updateBoard(BoardDto board) throws Exception;
+
+	void deleteBoard(int boardIdx) throws Exception;
+
+	BoardFileDto selectBoardFileInformation(int idx, int boardIdx) throws Exception;
+}
